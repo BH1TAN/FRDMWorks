a2dd852a67d6a1380556febb8999fc37d431c355
BH1TAN <yxtccty@163.com>
2015-05-11 18:28:49 +0800

    对蓝牙模块的调试
    TXD-PTE23(RX)
    RXD-PTE22(TX)

---
+++ HC05-Debug/FLASH/Sources/Events.d
@@ -0,0 +1,85 @@
+Sources/Events.o: ../Sources/Events.c \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/Cpu.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Types.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Error.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Const.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/IO_Map.h \
+ ../Sources/Events.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Types.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Error.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Const.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/IO_Map.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/AS1.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/ASerialLdd1.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/Cpu.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/ASerialLdd1.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/AS2.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/ASerialLdd2.h \
+ E:/Freescale/workspace/HC05-Debug/Generated_Code/ASerialLdd2.h
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/Cpu.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Types.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Error.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Const.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/IO_Map.h:
+
+../Sources/Events.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Types.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Error.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/PE_Const.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/IO_Map.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/AS1.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/ASerialLdd1.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/Cpu.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/ASerialLdd1.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/AS2.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/ASerialLdd2.h:
+
+E:/Freescale/workspace/HC05-Debug/Generated_Code/ASerialLdd2.h:
---
+++ HC05-Debug/Sources/Events.h
@@ -0,0 +1,96 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-05-03, 00:15, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "AS1.h"
+#include "ASerialLdd1.h"
+#include "AS2.h"
+#include "ASerialLdd2.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL25Z128LK4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void);
+
+
+void AD1_OnEnd(void);
+/*
+** ===================================================================
+**     Event       :  AD1_OnEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called after the measurement (which consists
+**         of <1 or more conversions>) is/are finished.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
